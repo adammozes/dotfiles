1e927c1e748f4ecca1c6d8c73885a936469140bb
Adam Mozes <adam.mozes@formelio.nl>
2025-04-01 16:55:12 +0200

    fixup! Never gonna say goodbye
    DISCLAIMER: this commit message has been retrieved from whatthecommit.com.

--- fish/config.fish
+++ fish/config.fish
@@ -77,5 +77,27 @@ if status --is-interactive
 
   fish_vi_key_bindings
 
+  function __direnv_export_eval --on-event fish_prompt;
+      "direnv" export fish | source;
+      if test "$direnv_fish_mode" != "disable_arrow";
+          function __direnv_cd_hook --on-variable PWD;
+              if test "$direnv_fish_mode" = "eval_after_arrow";
+                  set -g __direnv_export_again 0;
+              else;
+                  "direnv" export fish | source;
+              end;
+          end;
+      end;
+  end;
+  function __direnv_export_eval_2 --on-event fish_preexec;
+      if set -q __direnv_export_again;
+          set -e __direnv_export_again;
+          "direnv" export fish | source;
+          echo;
+      end;
+      functions --erase __direnv_cd_hook;
+  end;
+
   tmux attach; or tmux
+
 end
